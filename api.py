241dede7e8092c176e744c1dcf5720294b426cc8
stephan223344 <stephanckaert@gmail.com>
2026-03-15 12:24:42 +0100

    mety

--- api.py
+++ api.py
@@ -134,8 +134,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = build_message(prediction, market)
 
         keyboard = [
-            [InlineKeyboardButton("🔄Get Prediction", callback_data="market_{market}")]
-            [InlineKeyboardButton("🔥 Go to My Channel", url=f"https://t.me/{CHANNEL_ID.strip('@')}")],   # #callback_data=f"market_{market}
+            [InlineKeyboardButton("🔄 New prediction", callback_data=f"market_{market}")],   
             [InlineKeyboardButton("⬅️ Back", callback_data="menu")]
         ]
 
