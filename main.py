33a8ec794dbbd228afc4f0528eb451366e233731
stephan223344 <stephanckaert@gmail.com>
2026-04-02 08:58:08 +0200

    interval 1h

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def main():
     app.add_handler(CallbackQueryHandler(callback_handler))
     app.add_handler(MessageHandler(filters.Sticker.ALL, get_sticker_id))
 
-    app.job_queue.run_repeating(auto_post, interval=10000, first=10)
+    app.job_queue.run_repeating(auto_post, interval=3600, first=10)
 
     print("✅ Bot started (polling + Flask keep-alive)")
     app.run_polling(drop_pending_updates=True)
